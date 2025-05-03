c63819d25a999542765f5f2db7feea4fdbf91266
janietong <145235615+janietong@users.noreply.github.com>
2025-05-02 21:56:16 -0600

    refactor imports

--- app/issues/[id]/loading.tsx
+++ app/issues/[id]/loading.tsx
@@ -1,5 +1,4 @@
-import IssueStatusBadge from '@/app/components/IssueStatusBadge'
-import { Heading, Flex, Card, Box } from '@radix-ui/themes'
+import { Flex, Card, Box } from '@radix-ui/themes'
 import Skeleton from 'react-loading-skeleton'
 import 'react-loading-skeleton/dist/skeleton.css'
 
--- app/issues/loading.tsx
+++ app/issues/loading.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import IssueStatusBadge from '../components/IssueStatusBadge';
 import Skeleton from 'react-loading-skeleton';
 import 'react-loading-skeleton/dist/skeleton.css';
 import IssueActions from './IssueActions';
