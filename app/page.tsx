b1412a1660fe0c682a4a77403ba405a4001672a2
janietong <145235615+janietong@users.noreply.github.com>
2025-05-03 21:16:45 -0600

    layout dashboard

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,32 @@
-import Pagination from "./components/Pagination";
+import prisma from "@/prisma/client";
+import LatestIssues from "./LatestIssues";
+import IssueSummary from "./IssueSummary";
+import IssueChart from "./IssueChart";
+import { Grid, Flex } from "@radix-ui/themes";
+import { Metadata } from "next";
 
-export default function Home({searchParams}: {searchParams: {page: string}}) {
-  return (
-    <Pagination itemCount={100} pageSize={10} currentPage={parseInt(searchParams.page)} />
-  )
+export default async function Home() {
+const open = await prisma.issue.count({ where: { status: 'OPEN' } });
+const inProgress = await prisma.issue.count({ where: { status: 'IN_PROGRESS' } });
+const closed = await prisma.issue.count({ where: { status: 'CLOSED' } });
+const cancelled = await prisma.issue.count({ where: { status: 'CANCELLED' } });
+
+return (
+  <Grid columns={"1"} gap="5">
+    <IssueSummary
+      open={open}
+      inProgress={inProgress}
+      closed={closed}
+      cancelled={cancelled}
+    />
+    <LatestIssues />
+  </Grid>
+);
 }
+
+export const dynamic = 'force-dynamic'; 
+
+export const metadata: Metadata = {
+title: 'Issue Tracker - Dashboard',
+description: 'View a summary of project issues'
+};
